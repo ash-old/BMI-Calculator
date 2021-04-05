dd26a01ac58c370f71038b9e6c1c4a0bc346c0a1
ash-old <ashley.oldham@gmail.com>
2021-04-05 22:13:51 +0100

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Ash Oldham on 05/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
